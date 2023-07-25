3a3fbeebe80d014f9555eb633028f39ab72cf68b
Anirudh Singh <anirudh.singh@internshala.com>
2023-07-25 14:59:25 +0530

    changes

---
+++ javascript/index.js
@@ -0,0 +1,30 @@
+// const animatedBox = document.getElementById("profile-pic-div");
+
+// function isElementInViewport(element) {
+//   const rect = element.getBoundingClientRect();
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// }
+
+// function animateOnScroll() {
+//   if (isElementInViewport(animatedBox)) {
+//     // let a = document.getElementById('name-card');
+//     // console.log(a.classList);
+//     // a.classList.remove('name-card-animation');
+//     // void a.offsetWidth;
+//     // a.classList.add('name-card-animation');
+//     // // animatedBox.style.opacity = .5  ;
+//     // console.log(a.classList);
+//     // console.log('hi');
+//   }
+// }
+
+// // Initial check when the page loads
+// animateOnScroll();
+
+// // Listen for scroll events and trigger the animation function
+// window.addEventListener("scroll", animateOnScroll);
